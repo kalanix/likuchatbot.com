2f6ddcb52b12f07056f276aa8cbbc8e9dbb13c4f
kalanix <134939862+kalanix@users.noreply.github.com>
2023-05-29 16:50:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("chat-form");
 const input = document.getElementById("chat-input");
 const messages = document.getElementById("chat-messages");
-const apiKey = "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";
+const apiKey = "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";
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
